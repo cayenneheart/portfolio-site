aee00977c78c72c6fd6e458438cce839b64ee3cf
小峯海円 <kaien@mac.lan>
2026-03-09 15:49:45 +0900

    fix: ドメインをkaien.mai-mee.comに修正

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,6 @@
 import { MetadataRoute } from 'next';
 
-const BASE_URL = process.env.NEXT_PUBLIC_SITE_URL || 'https://kaien.maimee.com';
+const BASE_URL = process.env.NEXT_PUBLIC_SITE_URL || 'https://kaien.mai-mee.com';
 
 export default function robots(): MetadataRoute.Robots {
   return {
